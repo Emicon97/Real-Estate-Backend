7ac854a39975c247a15bca7caa9925e559bfda04
juankjota <jcardev7@gmail.com>
2022-06-25 12:58:16 -0500

    creacion de la ruta get y post del usuario

--- api/src/controllers/userControllers.ts
+++ api/src/controllers/userControllers.ts
@@ -1,5 +1,23 @@
 import userModel from "../models/users.";
 import { User, UserType } from './../models/users.';
 
+async function getAllUsers(info:User):Promise<User[]>{
+    const allUsers:User[] = await userModel.find();
+    
+    if(allUsers.length){
+     return allUsers; 
+    }
+    
+    throw new Error("No se encontraron usuarios.");
+ }
 
-export {}
+async function createUser(data:User):Promise<User>{
+    const user:UserType = await userModel.create(data);
+
+    const savedUser:User = await user.save();
+    return savedUser;    
+}
+
+export {createUser,
+        getAllUsers,            
+}
--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,10 +1,29 @@
 import { Router } from 'express';
+import { read } from 'fs';
+import { getAllUsers, createUser } from '../controllers/userControllers';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
    try{
+      const info = req.body;
+      const data = await getAllUsers(info);
+      res.json(data);
+   }catch(error:any){
+      if (error instanceof Error) {
+         console.log(error.message);
+         res.status(404).json(error);
+      } else {
+         console.log('Unexpected Error', error);
+      }
+   }
+})
 
+router.post('/', async(req, res) => {
+   try{
+      const data = req.body;
+      const property = await createUser(data);
+      res.status(201).send(property)  
    }catch(error:any){
       if (error instanceof Error) {
          console.log(error.message);
