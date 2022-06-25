2b5f2fcf1b612c56cee25986374126cfcb0b35a3
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-25 04:07:34 -0300

    filters and search

--- api/dist/src/controllers/propertyControllers.js
+++ api/dist/src/controllers/propertyControllers.js
@@ -12,8 +12,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateProperty = exports.deleteProperty = exports.getPropById = exports.getAllProperties = exports.createProperty = void 0;
+exports.updateProperty = exports.deleteProperty = exports.getPropById = exports.getPropertyManager = exports.createProperty = void 0;
 const properties_1 = __importDefault(require("../models/properties"));
+function getPropertyManager(filters, location) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const allProperties = yield getAllProperties();
+        if (filters && location) {
+            const filtered = yield searchByFilter(filters);
+            const searched = yield searchByLocation(location, filtered);
+            return searched;
+        }
+        else if (filters) {
+            const filtered = yield searchByFilter(filters);
+            return filtered;
+        }
+        else if (location) {
+            const searched = yield searchByLocation(location, allProperties);
+            return searched;
+        }
+        else {
+            return allProperties;
+        }
+    });
+}
+exports.getPropertyManager = getPropertyManager;
 function getAllProperties() {
     return __awaiter(this, void 0, void 0, function* () {
         const allProperties = yield properties_1.default.find();
@@ -23,7 +45,35 @@ function getAllProperties() {
         throw new Error("No se encontraron propiedades.");
     });
 }
-exports.getAllProperties = getAllProperties;
+function searchByFilter(filtered) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const property = yield properties_1.default.find(filtered);
+        return property;
+    });
+}
+function searchByLocation(location, properties) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const toFilter = [];
+        const names = location.trim().split(' ');
+        properties.forEach((property) => {
+            names.forEach((word) => {
+                var _a;
+                if (word.length) {
+                    if (!toFilter.includes(property) &&
+                        property.city.includes(word)) {
+                        toFilter.push(property);
+                    }
+                    else if (!toFilter.includes(property) &&
+                        ((_a = property.neighbourhood) === null || _a === void 0 ? void 0 : _a.includes(word)) &&
+                        property.neighbourhood !== 'No especificado') {
+                        toFilter.push(property);
+                    }
+                }
+            });
+        });
+        return toFilter;
+    });
+}
 function getPropById(id) {
     return __awaiter(this, void 0, void 0, function* () {
         const propById = yield properties_1.default.findById(id);
@@ -34,28 +84,21 @@ function getPropById(id) {
     });
 }
 exports.getPropById = getPropById;
-function createProperty({ address, area, type, rooms, city, bathrooms, neighbourhood, constructionDate, renovationDate, parkingSlot, rentPrice, sellPrice, pictures }) {
+function createProperty(data) {
     return __awaiter(this, void 0, void 0, function* () {
-        const property = yield properties_1.default.create({
-            address,
-            area,
-            type,
-            rooms,
-            city,
-            bathrooms,
-            neighbourhood,
-            constructionDate: constructionDate ? constructionDate : undefined,
-            renovationDate: renovationDate ? renovationDate : undefined,
-            parkingSlot,
-            rentPrice,
-            sellPrice,
-            pictures: pictures && pictures.length ? pictures : undefined
-        });
+        const property = yield properties_1.default.create(data);
         const savedProperty = yield property.save();
         return savedProperty;
     });
 }
 exports.createProperty = createProperty;
+function updateProperty(_id, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield properties_1.default.findOneAndUpdate({ _id }, data, { new: true });
+        return 'Propiedad actualizada con éxito.';
+    });
+}
+exports.updateProperty = updateProperty;
 function deleteProperty(id) {
     return __awaiter(this, void 0, void 0, function* () {
         yield properties_1.default.findByIdAndDelete(id);
@@ -63,24 +106,3 @@ function deleteProperty(id) {
     });
 }
 exports.deleteProperty = deleteProperty;
-function updateProperty(_id, { address, area, type, rooms, city, bathrooms, neighbourhood, constructionDate, renovationDate, parkingSlot, rentPrice, sellPrice, pictures }) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield properties_1.default.findOneAndUpdate({ _id }, {
-            address,
-            area,
-            type,
-            rooms,
-            city,
-            bathrooms,
-            neighbourhood,
-            constructionDate,
-            renovationDate,
-            parkingSlot,
-            rentPrice,
-            sellPrice,
-            pictures
-        }, { new: true });
-        return 'Propiedad actualizada con éxito.';
-    });
-}
-exports.updateProperty = updateProperty;
