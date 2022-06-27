97c3165389534801d65e0c68122ff0f8d57018e3
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-27 18:02:36 -0300

    fixed tscofig

--- api/dist/src/index.js
+++ api/dist/src/index.js
@@ -13,10 +13,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const config_json_1 = __importDefault(require("../config.json"));
 const index_1 = __importDefault(require("./db/index"));
 // Start the application by listening to specific port
-const port = Number(process.env.PORT || config_json_1.default.PORT || 3001);
+const port = Number(process.env.PORT || 3001);
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, index_1.default)();
     app_1.default.listen(port, () => {
