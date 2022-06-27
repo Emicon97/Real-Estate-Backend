38cd5080281e4be609d76f1a9ea50d7dbd88c631
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-27 17:46:19 -0300

    no config.json 3

---
+++ api/dist/controllers/propertyControllers.js
@@ -0,0 +1,115 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateProperty = exports.deleteProperty = exports.getPropById = exports.getPropertyManager = exports.createProperty = void 0;
+const properties_1 = __importDefault(require("../models/properties"));
+function getPropertyManager(filters, location, max) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const allProperties = yield getAllProperties();
+        if ((filters && Object.keys(filters).length) && location) {
+            const filtered = yield searchByFilter(filters, max);
+            const searched = yield searchByLocation(location, filtered);
+            return searched;
+        }
+        else if ((filters && Object.keys(filters).length)) {
+            const filtered = yield searchByFilter(filters, max);
+            return filtered;
+        }
+        else if (location) {
+            const searched = yield searchByLocation(location, allProperties);
+            return searched;
+        }
+        else {
+            return allProperties;
+        }
+    });
+}
+exports.getPropertyManager = getPropertyManager;
+function getAllProperties() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const allProperties = yield properties_1.default.find();
+        if (allProperties.length) {
+            return allProperties;
+        }
+        throw new Error("No se encontraron propiedades.");
+    });
+}
+function searchByFilter(filtered, max) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (max) {
+            const property = yield properties_1.default.find(filtered)
+                .where('price').gt(0).lt(max);
+            return property;
+        }
+        else {
+            const property = yield properties_1.default.find(filtered);
+            return property;
+        }
+    });
+}
+function searchByLocation(location, properties) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const toFilter = [];
+        const names = location.trim().split(' ');
+        properties.forEach((property) => {
+            names.forEach((word) => {
+                var _a;
+                if (word.length) {
+                    if (!toFilter.includes(property) &&
+                        property.city.includes(word)) {
+                        toFilter.push(property);
+                    }
+                    else if (!toFilter.includes(property) &&
+                        ((_a = property.neighbourhood) === null || _a === void 0 ? void 0 : _a.includes(word)) &&
+                        property.neighbourhood !== 'No especificado') {
+                        toFilter.push(property);
+                    }
+                }
+            });
+        });
+        return toFilter;
+    });
+}
+function getPropById(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const propById = yield properties_1.default.findById(id);
+        if (propById !== null) {
+            return propById;
+        }
+        throw new Error("Esta propiedad no está disponible.");
+    });
+}
+exports.getPropById = getPropById;
+function createProperty(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const property = yield properties_1.default.create(data);
+        const savedProperty = yield property.save();
+        return savedProperty;
+    });
+}
+exports.createProperty = createProperty;
+function updateProperty(_id, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield properties_1.default.findOneAndUpdate({ _id }, data, { new: true });
+        return 'Propiedad actualizada con éxito.';
+    });
+}
+exports.updateProperty = updateProperty;
+function deleteProperty(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield properties_1.default.findByIdAndDelete(id);
+        return 'Propiedad eliminada con éxito.';
+    });
+}
+exports.deleteProperty = deleteProperty;
---
+++ api/dist/models/users..js
@@ -0,0 +1,91 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const typegoose_1 = require("@typegoose/typegoose");
+const properties_1 = require("./properties");
+var Range;
+(function (Range) {
+    Range["free"] = "free";
+    Range["premium"] = "premium";
+    Range["vip"] = "vip";
+    Range["admin"] = "admin";
+})(Range || (Range = {}));
+;
+class User {
+}
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", String)
+], User.prototype, "name", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", String)
+], User.prototype, "lastName", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", String)
+], User.prototype, "password", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", Date)
+], User.prototype, "birthday", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", String)
+], User.prototype, "email", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", Number)
+], User.prototype, "dni", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true }),
+    __metadata("design:type", Number)
+], User.prototype, "telephone", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ required: true, enum: Range, default: 'free' }),
+    __metadata("design:type", String)
+], User.prototype, "range", void 0);
+__decorate([
+    (0, typegoose_1.prop)(),
+    __metadata("design:type", String)
+], User.prototype, "avatar", void 0);
+exports.User = User;
+class Free extends User {
+}
+__decorate([
+    (0, typegoose_1.prop)({ ref: () => properties_1.Property }),
+    __metadata("design:type", Array)
+], Free.prototype, "favourites", void 0);
+class Premium extends User {
+}
+__decorate([
+    (0, typegoose_1.prop)({ ref: () => properties_1.Property }),
+    __metadata("design:type", Array)
+], Premium.prototype, "properties", void 0);
+class Vip extends User {
+}
+__decorate([
+    (0, typegoose_1.prop)({ ref: () => properties_1.Property }),
+    __metadata("design:type", Array)
+], Vip.prototype, "properties", void 0);
+class Admin extends User {
+}
+__decorate([
+    (0, typegoose_1.prop)({ enum: Range }),
+    __metadata("design:type", String)
+], Admin.prototype, "userType", void 0);
+__decorate([
+    (0, typegoose_1.prop)({ refPath: 'userType' }),
+    __metadata("design:type", Object)
+], Admin.prototype, "sender", void 0);
+const userModel = (0, typegoose_1.getModelForClass)(User);
+exports.default = userModel;
