28b1a3e08f570c8ccd9978c250c08b21e01901c5
EmiCon97 <derleuchtturm97@gmail.com>
2022-06-27 17:41:35 -0300

    no config.json 2

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,8 @@
 import app from './app';
-import config from '../config.json';
 import dbConnection  from './db/index';
 
 // Start the application by listening to specific port
-const port = Number(process.env.PORT || config.PORT || 3001);
+const port = Number(process.env.PORT || 3001);
 
 const main = async () => {
   await dbConnection();
